3b6ac6f488937c26bf6eac42024ce69c8039bbd9
Nouman Coding <noumanali.coding@gmail.com>
2025-07-12 11:42:05 +0500

    add: select DOM elements for count and buttons

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // Counter logic will be implemented here
+let count = 0;
+const countdisplay = document.getElementById('count');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const restBtn = document.getElementById('rest');
