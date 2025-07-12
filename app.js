4f29fbbb8ef659ba524e8f66007f8590bd3ff8d1
Nouman Coding <noumanali.coding@gmail.com>
2025-07-12 11:50:34 +0500

    feat: implement increment logic

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const countdisplay = document.getElementById('count');
 const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
 const restBtn = document.getElementById('rest');
+
+increaseBtn.addEventListener('click', () => {
+  count++;
+  countdisplay.textContent = count;
+});
